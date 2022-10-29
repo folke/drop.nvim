8cbd41c6ed3163aca6dd90cd2ee8720202c51412
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-10-29 22:23:47 +0200

    perf: use one window and extmarks to render the drops

--- lua/drop/drop.lua
+++ lua/drop/drop.lua
@@ -6,23 +6,28 @@ local M = {}
 M.timer = nil
 
 ---@class Drop
----@field id integer
 ---@field symbol string
 ---@field hl_group string
 ---@field row integer
 ---@field col integer
----@field win? integer
 ---@field buf? integer
----@field hidden boolean
 ---@field speed integer
+---@field id? integer
 local Drop = {}
 Drop.__index = Drop
 
 ---@type table<integer, Drop>
 Drop.drops = {}
 
-function Drop.new()
+function Drop.new(buf)
   local self = setmetatable({}, Drop)
+  self.buf = buf
+  self.id = nil
+  self:init()
+  return self
+end
+
+function Drop:init()
   local theme = config.get_theme()
   local symbols = theme.symbols
   local colors = vim.tbl_keys(theme.colors)
@@ -31,44 +36,15 @@ function Drop.new()
   self.hl_group = "Drop" .. math.random(1, #colors) .. (math.random(1, 3) == 1 and "Bold" or "")
   self.row = 0
   self.col = math.random(0, vim.go.columns)
-  self.win = nil
-  self.buf = nil
   self.speed = math.random(1, 2)
-  self.hidden = false
-  return self
 end
 
 function Drop:show()
-  if not self.win then
-    self.buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(self.buf, 0, -1, false, { self.symbol })
-    self.win = vim.api.nvim_open_win(self.buf, false, {
-      relative = "editor",
-      row = self.row,
-      col = self.col,
-      width = vim.fn.strwidth(self.symbol) + 1,
-      height = 1,
-      focusable = false,
-      zindex = 1000,
-      style = "minimal",
-      noautocmd = true,
-    })
-    vim.wo[self.win].winhighlight = "NormalFloat:" .. self.hl_group
-  else
-    vim.api.nvim_win_set_config(self.win, {
-      relative = "editor",
-      row = self.row,
-      col = self.col,
-    })
-  end
-end
-
-function Drop:hide()
-  self.hidden = true
-  if self.win then
-    pcall(vim.api.nvim_win_close, self.win, true)
-    pcall(vim.api.nvim_buf_delete, self.buf, { force = true })
-  end
+  self.id = vim.api.nvim_buf_set_extmark(self.buf, config.ns, self.row, 0, {
+    virt_text = { { self.symbol, self.hl_group } },
+    virt_text_win_col = self.col,
+    id = self.id,
+  })
 end
 
 function Drop:is_visible()
@@ -80,20 +56,37 @@ function Drop:update()
   self.row = self.row + self.speed
   self.col = self.col + dx
 
-  if self:is_visible() then
-    self:show()
-  else
-    self:hide()
+  if not self:is_visible() then
+    self:init()
   end
-  return not self.hidden
+
+  self:show()
 end
 
 M.ticks = 0
+M.buf = nil
+M.win = nil
 
 function M.show()
   if M.timer then
     return
   end
+  M.buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(M.buf, 0, -1, false, vim.split(string.rep("\n", vim.go.lines), "\n"))
+
+  M.win = vim.api.nvim_open_win(M.buf, false, {
+    relative = "editor",
+    row = 0,
+    col = 0,
+    width = vim.go.columns,
+    height = vim.go.lines,
+    focusable = false,
+    zindex = 1000,
+    style = "minimal",
+    noautocmd = true,
+  })
+  vim.wo[M.win].winhighlight = "NormalFloat:Drop"
+  vim.wo[M.win].winblend = config.options.winblend
   M.ticks = 0
   M.timer = vim.defer_fn(M.update, config.options.interval)
 end
@@ -102,33 +95,30 @@ function M.update()
   if M.timer == nil then
     return
   end
+
   M.ticks = M.ticks + 1
   local pct = math.min(vim.go.lines, M.ticks) / vim.go.lines
   local _target = pct * config.options.max
   vim.go.lazyredraw = true
   while #Drop.drops < _target * math.random(80, 100) / 100 do
-    table.insert(Drop.drops, Drop.new())
+    table.insert(Drop.drops, Drop.new(M.buf))
   end
-  Drop.drops = vim.tbl_filter(
-    ---@param drop Drop
-    function(drop)
-      return drop:update()
-    end,
-    Drop.drops
-  )
-  vim.go.lazyredraw = false
+
+  for _, drop in ipairs(Drop.drops) do
+    drop:update()
+  end
   M.timer = vim.defer_fn(M.update, config.options.interval)
 end
 
 function M.hide()
+  vim.go.lazyredraw = false
   if not M.timer then
     return
   end
   M.timer:stop()
   M.timer = nil
-  for _, drop in ipairs(Drop.drops) do
-    drop:hide()
-  end
+  pcall(vim.api.nvim_win_close, M.win, true)
+  pcall(vim.api.nvim_buf_delete, M.buf, { force = true })
   Drop.drops = {}
 end
 
